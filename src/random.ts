1a45474b1478b947d79231439703d0473d9b6893
crossjs <liwenfu@crossjs.com>
2019-12-21 13:31:52 +0800

    0.0.3

--- src/random.ts
+++ src/random.ts
@@ -1,6 +1,8 @@
 import { Encrypt } from "./encrypt";
 
 const { encode } = Encrypt.create();
+const seed = Number.MAX_SAFE_INTEGER - 1e15 - 1e14;
+
 export function random() {
   return encode(1e15 + Math.floor(Math.random() * seed) + Date.now());
 }
